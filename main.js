30805fc4613e1579beedeb1e994231e82ee51511
Alicia Miceli <aliciamiceli@Alicias-MacBook-Pro.local>
2016-08-22 20:31:08 -0500

    commit

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 console.log(SolarSystem);
 
-console.log("The planets are ", SolarSystem.getPlanets());
-console.log("New planet:", SolarSystem.setPlanets("YourMother"));
-console.log("New planet:", SolarSystem.getPlanets());
+// console.log("The planets are ", SolarSystem.getPlanets());
+// console.log("New planet:", SolarSystem.setPlanets("YourMother"));
+// console.log("New planet:", SolarSystem.getPlanets());
 
-console.log("Our active spacecraft includes:", SolarSystem.getActiveSpaceCraft());
-console.log(SolarSystem.setActiveSpaceCraft("yellow"));
-console.log("Our newest spacecraft is ", SolarSystem.getActiveSpaceCraft());
+// console.log("Our active spacecraft includes:", SolarSystem.getActiveSpaceCraft());
+// console.log(SolarSystem.setActiveSpaceCraft("yellow"));
+// console.log("Our newest spacecraft is ", SolarSystem.getActiveSpaceCraft());
 
-console.log("We have landed on ", SolarSystem.getPlanetsLandedOn(), " planets");
-console.log(SolarSystem.setPlanetsLandedOn(4));
-console.log("Update: we have landed on ", SolarSystem.getPlanetsLandedOn(), " planets");
+// console.log("We have landed on ", SolarSystem.getPlanetsLandedOn(), " planets");
+// console.log(SolarSystem.setPlanetsLandedOn(4));
+// console.log("Update: we have landed on ", SolarSystem.getPlanetsLandedOn(), " planets");
+
+
+console.log(SolarSystem.getClosestStars());
